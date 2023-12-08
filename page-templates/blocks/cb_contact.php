cd5b25da057406e8716cf379cfe01e737019d0b9
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-08 15:13:47 +0000

    list styling

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -5,7 +5,7 @@
                 <h2>Contact Us</h2>
                 <ul class="fa-ul">
                     <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-phone"></i></span> <a href="tel:<?=parse_phone(get_field('contact_phone','options'))?>"><?=get_field('contact_phone','options')?></a></li>
-                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-map-marer-alt"></i></span> <?=get_field('contact_address','options')?></li>
+                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span> <?=get_field('contact_address','options')?></li>
                 </ul>
             </div>
             <div class="col-md-6"><iframe src="<?=get_field('maps_url','options')?>" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
