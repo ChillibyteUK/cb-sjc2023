c7e0d2eda4cfe68b7846327476230b549e99cd31
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-08 15:02:16 +0000

    added contact block

---
+++ page-templates/blocks/cb_contact.php
@@ -0,0 +1,8 @@
+<section class="contact">
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-md-6">ADDR</div>
+            <div class="col-md-6">MAP</div>
+        </div>
+    </div>
+</section>
