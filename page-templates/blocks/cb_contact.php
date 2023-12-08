1dcc7e8dcfff0f600ec213dcfc8786bd4af67cd9
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-08 15:15:55 +0000

    Update cb_contact.php

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -5,6 +5,7 @@
                 <h2>Contact Us</h2>
                 <ul class="fa-ul">
                     <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-phone has-accent-400-color"></i></span> <a href="tel:<?=parse_phone(get_field('contact_phone','options'))?>"><?=get_field('contact_phone','options')?></a></li>
+                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-envelope has-accent-400-color"></i></span> <a href="mailto:<?=get_field('contact_email','options')?>"><?=get_field('contact_email','options')?></a></li>
                     <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-map-marker-alt has-accent-400-color"></i></span> <?=get_field('contact_address','options')?></li>
                 </ul>
             </div>
