f090f396f3239dcf24b3648f923f3f747992b7ad
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-08 15:03:38 +0000

    Update cb_contact.php

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -3,6 +3,9 @@
         <div class="row">
             <div class="col-md-6">ADDR</div>
             <div class="col-md-6">MAP</div>
+            <div class="col-12">
+            <?=do_shortcode('[gravityform id="1" title="false"]')?>
+            </div>
         </div>
     </div>
 </section>
