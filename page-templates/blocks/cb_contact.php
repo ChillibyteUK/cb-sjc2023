b911db809ff71cd7a04a46a6d09b80ea035c6927
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-08 15:06:36 +0000

    maps url to site-wide settings

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -2,8 +2,9 @@
     <div class="container-xl">
         <div class="row">
             <div class="col-md-6">ADDR</div>
-            <div class="col-md-6">MAP</div>
+            <div class="col-md-6"><iframe src="<?=get_field('maps_url','options')?>" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
             <div class="col-12">
+                <h2>Loan Enquiry</h2>
             <?=do_shortcode('[gravityform id="1" title="false"]')?>
             </div>
         </div>
